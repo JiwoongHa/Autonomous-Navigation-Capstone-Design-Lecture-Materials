1d23e5b5be9f4025ecc453d6117a2be4525ade2b
JiwoongHa <nb1031618@gmail.com>
2026-05-13 15:11:23 +0900

    Add files via upload

---
+++ LiDAR_Collision_Avoidance_Example/simulink/Init_collision_avoidance_UGV.m
@@ -0,0 +1,28 @@
+close all; clc; clear;
+
+addpath(genpath("D:\Jiung\matlab\대학원\2026-1학기\자율운항캡스톤디자인\tools"));
+
+% Chungnam Univ. Building E5-1
+ref_lat = 36.365227;
+ref_lon = 127.345838;
+
+ref_lla = [36.365227, 127.345838, 25.1110]; % CNU
+% ref_lla = [36.395991, 127.401794, 25.1110]; % SHI Square 33
+
+[x_ref,y_ref,~] = deg2utm(ref_lat,ref_lon);
+
+% RC min-max values
+RC_max = 2000;
+RC_min = 1000;
+
+Fx_max = 1;
+Fx_min = -1;
+
+N_max = 1;
+N_min = -1;
+
+d2r = pi/180;
+r2d = 180/pi;
+
+delta_min = -1;
+delta_max = 1;
